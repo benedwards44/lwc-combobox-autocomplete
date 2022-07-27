687cc6714bb5c7dbc0114eade9215e1f44c70b9c
Curtis Ward <cdw0035@mix.wvu.edu>
2022-07-27 12:44:59 -0400

    Moved closing logic from onBlur to onClick

--- src/lwc/comboboxAutocomplete/comboboxAutocomplete.js
+++ src/lwc/comboboxAutocomplete/comboboxAutocomplete.js
@@ -9,11 +9,23 @@ export default class ComboboxAutocomplete extends LightningElement {
     @api options;
     
     @track isFocussed = false;
+    @track isOpen = false;
 
     filteredOptions = [];
+    domElement;
+
+    constructor() {
+        super();
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    }
 
     connectedCallback() {
         this.filteredOptions = [...this.options];
+        document.addEventListener('click', this.handleOutsideClick);
+    }
+
+    disconnectedCallback() {
+        document.removeEventListener('click', this.handleOutsideClick);
     }
 
     filterOptions(event) {
@@ -37,6 +49,7 @@ export default class ComboboxAutocomplete extends LightningElement {
 
         // Close the picklist options
         this.isFocussed = false;
+        this.isOpen = false;
     }
 
     get noOptions() {
@@ -48,20 +61,33 @@ export default class ComboboxAutocomplete extends LightningElement {
         let dropdownClasses = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click';
         
         // Show dropdown list on focus
-        if (this.isFocussed) {
+        if (this.isOpen) {
             dropdownClasses += ' slds-is-open';
         }
 
         return dropdownClasses;
     }
 
+    handleOutsideClick(event) {
+
+        if ((!this.isFocussed) && (this.isOpen)) { 
+
+            //Fetch the dropdown DOM node
+            let domElement = this.template.querySelector('div[data-id="resultBox"]');
+
+            //Is the clicked element within the dropdown 
+            if (domElement && !domElement.contains(event.target)) {
+                this.isOpen = false;
+            }
+        }
+    }
+
     handleFocus() {
         this.isFocussed = true;
+        this.isOpen = true;
     }
 
     handleBlur() {
-        // Timeout to ensure click event is captured before the 
-        // options are hidden
-        setTimeout(() => { this.isFocussed = false; }, 500);
+        this.isFocussed = false;
     }
 }
